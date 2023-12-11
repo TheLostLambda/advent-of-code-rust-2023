c9391abd13465c978627f20af8e47d77e411b897
Brooks J Rady <b.j.rady@gmail.com>
2023-12-11 15:24:54 -0700

    Do Day 1, Part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,16 +1,49 @@
+use once_cell::sync::Lazy;
+use regex::Regex;
+
 advent_of_code::solution!(1);
 
-fn process_line(line: &str) -> Option<u32> {
-    let digits: Vec<u32> = line.chars().filter_map(|c| char::to_digit(c, 10)).collect();
+// OPTIMISATION: This is around 380x faster than building the regex fresh for every line!
+static DIGIT_REGEX: Lazy<Regex> =
+    Lazy::new(|| Regex::new(r"one|two|three|four|five|six|seven|eight|nine|\d").unwrap());
+
+fn process_line_one(line: &str) -> Option<u32> {
+    let digits: Vec<u32> = line.chars().filter_map(|c| c.to_digit(10)).collect();
     Some(10 * digits.first()? + digits.last()?)
 }
 
+fn convert_digit(digit_str: &str) -> Option<u32> {
+    Some(match digit_str {
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        c => c.parse().ok()?,
+    })
+}
+
+fn process_line_two(line: &str) -> Option<u32> {
+    let first_digit = convert_digit(DIGIT_REGEX.find(line)?.as_str())?;
+    let last_digit = convert_digit(
+        (0..line.len())
+            .rev()
+            .find_map(|i| DIGIT_REGEX.find(&line[i..]))?
+            .as_str(),
+    )?;
+    Some(10 * first_digit + last_digit)
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    Some(input.lines().filter_map(process_line).sum())
+    Some(input.lines().filter_map(process_line_one).sum())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    Some(input.lines().filter_map(process_line_two).sum())
 }
 
 #[cfg(test)]
@@ -25,7 +58,13 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(281));
+        assert_eq!(
+            process_line_two("qccnqvfnkkkvsixktsixnine1twoneq"),
+            Some(61)
+        );
     }
 }
